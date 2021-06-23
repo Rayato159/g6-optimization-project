65fd5ee3277a5b7be886cf7c5e6cdd20c518182c
Rayato159 <rayato159@gmail.com>
2021-06-24 04:29:29 +0700

    exam2 complete.

--- src/exam_2_Solution.py
+++ src/exam_2_Solution.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from Genetic_Algorithm import GA
 import time
 
-GA_model = GA(chromosome_size=16, population=200, cross_prob=0.9, muta_prob=0.3)
+GA_model = GA(chromosome_size=16, population=25, cross_prob=0.9, muta_prob=0.1)
 
 population = GA_model.population
 generation = 100
@@ -45,7 +45,7 @@ for i in range(generation):
         print(f"Obj_value_for_mutated_chlid #2 at generation #{gen} : {obj_val_muta_chlid_2}")
 
         mutated_1_with_obj_val = np.hstack((obj_val_muta_chlid_1, muta_chlid_1))
-        mutated_2_with_obj_val = np.hstack((obj_val_muta_chlid_2, muta_chlid_1))
+        mutated_2_with_obj_val = np.hstack((obj_val_muta_chlid_2, muta_chlid_2))
 
         new_population = np.vstack((new_population, muta_chlid_1, muta_chlid_2))
 
@@ -63,5 +63,39 @@ for i in range(generation):
     best_of_a_generation = np.vstack((best_of_a_generation,
                                         sorted_best_for_plotting[0]))
 
+    print()
+    for i in pool_of_solution:
+        print(GA_model.getObjective(i))
+
     gen += 1
 
+end_time = time.time()
+
+sorted_last_population = np.array(sorted(new_population_with_obj_value, 
+                                            key=lambda x:x[0]))
+
+sorted_best_of_generation = np.array(sorted(best_of_a_generation, 
+                                            key=lambda x:x[0]))
+
+best_convergence = sorted_last_population[0]
+best_overall = sorted_best_of_generation[0]
+
+print("\n")
+print("--------------------------------")
+print(f"Execution time: {end_time-start_time}")
+print()
+print(f"Final solution (Convergence): {best_convergence[1:]}")
+print(f"Encode_x1 (Convergence): {best_convergence[9:]}")
+print(f"Encode_x2 (Convergence): {best_convergence[1:9]}")
+print()
+print(f"Final solution (Best): {best_overall[1:]}")
+print(f"Encode_x1 (Best): {best_overall[9:]}")
+print(f"Encode_x2 (Best): {best_overall[1:9]}")
+print("\n")
+print(f"Final obj value (Convergence) {GA_model.getObjective(best_convergence)[2]}")
+print(f"Decode_x2 (Convergence): {GA_model.getObjective(best_convergence)[1]}")
+print(f"Decode_x1 (Convergence): {GA_model.getObjective(best_convergence)[0]}")
+print()
+print(f"Final Obj (Best): {GA_model.getObjective(best_overall)[2]}")
+print(f"Decode_x2 (Best): {GA_model.getObjective(best_overall)[1]}")
+print(f"Decode_x1 (Best): {GA_model.getObjective(best_overall)[0]}")
