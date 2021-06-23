13465753634a0af2e988c7a3985d4547e4bb2ed6
Rayato159 <rayato159@gmail.com>
2021-06-24 00:16:52 +0700

    Mutation complete

--- src/exam_2_solution.py
+++ src/exam_2_solution.py
@@ -77,9 +77,99 @@ def find_parents_ts(all_solution):
             
         print(f"winner is {selected_parent}")
         print(f"minimum value is {min_obj_func}")
+        print("--------------------------------------------------")
         print()
 
         parents = np.vstack((parents, selected_parent))
+    
+    parent_1 = parents[0,:]
+    parent_2 = parents[1,:]
+
+    return (parent_1, parent_2)
+
+#Crossover
+def crossover(parent_1, parent_2, cross_prob=0.8):
+
+    chlid_1 = np.empty((0, len(parent_1)))
+    chlid_2 = np.empty((0, len(parent_2)))
+
+    cross_rand_prob = np.random.rand()
+
+    if cross_rand_prob < cross_prob:
+
+        index_1 = np.random.randint(0, len(parent_1))
+        index_2 = np.random.randint(0, len(parent_2))
+
+        while index_1 == index_2:
+            index_2 = np.random.randint(0, len(parent_2))
+
+        if index_1 > index_2:
+            index_1, index_2 = index_2, index_1
+        
+        #Parent_1
+        first_sec_par_1 = parent_1[:index_1]
+        mid_sec_par_1 = parent_1[index_1:index_2+1]
+        last_sec_par_1 = parent_1[index_2+1:]
+        
+        #Parent_2
+        first_sec_par_2 = parent_2[:index_1]
+        mid_sec_par_2 = parent_2[index_1:index_2+1]
+        last_sec_par_2 = parent_2[index_2+1:]
+
+        chlid_1 = np.concatenate((first_sec_par_1, mid_sec_par_2, last_sec_par_1))
+        chlid_2 = np.concatenate((first_sec_par_2, mid_sec_par_1, last_sec_par_2))
+
+    else:
+        chlid_1 = parent_1
+        chlid_2 = parent_2
+
+    return (chlid_1, chlid_2)
+
+def mutation(chlid_1, chlid_2, muta_prob=0.2):
+    
+    #Chlid_1
+    mutated_chlid_1 = np.empty((0, len(chlid_1)))
+
+    t = 0
+    for i in chlid_1:
+        muta_rand_prob = np.random.rand()
+
+        if muta_rand_prob < muta_prob:
+
+            if chlid_1[t] == 0:
+                chlid_1[t] = 1
+            else:
+                chlid_1[t] = 0
+
+            mutated_chlid_1 = chlid_1
+            t += 1
+
+        else:
+            mutated_chlid_1 = chlid_1
+            t += 1
+
+    #Chlid_2
+    mutated_chlid_2 = np.empty((0, len(chlid_2)))
+
+    t = 0
+    for i in chlid_2:
+        muta_rand_prob = np.random.rand()
+
+        if muta_rand_prob < muta_prob:
+
+            if chlid_2[t] == 0:
+                chlid_2[t] = 1
+            else:
+                chlid_2[t] = 0
+
+            mutated_chlid_2 = chlid_2
+            t += 1
+        
+        else:
+            mutated_chlid_2 = chlid_2
+            t += 1
+
+    return (mutated_chlid_1, mutated_chlid_2)
 
 crack = Black_Box_Function("input2.txt", "output2.txt", "2-6.exe")
 
@@ -94,3 +184,27 @@ for i in range(population):
     all_solution = np.vstack((all_solution, chromosome))
 
 print(all_solution, end="\n\n")
+
+parents = find_parents_ts(all_solution)
+parent_1 = parents[0]
+parent_2 = parents[1]
+
+print(f"parent1# {parent_1}")
+print(f"parent2# {parent_2}")
+print()
+
+chlids = crossover(parent_1, parent_2)
+chlid_1 = chlids[0]
+chlid_2 = chlids[1]
+
+print(f"chlid1# {chlid_1}")
+print(f"chlid2# {chlid_2}")
+print()
+
+mutated_chlids = mutation(chlid_1, chlid_2)
+mutated_chlid_1 = mutated_chlids[0]
+mutated_chlid_2 = mutated_chlids[1]
+
+print(f"mutated_chlid1# {mutated_chlid_1}")
+print(f"mutated_chlid2# {mutated_chlid_2}")
+print()
